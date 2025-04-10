f4e3a1a6eb648a7aa9476ac9015ded5d541a849d
InomRai <monirai849@gmail.com>
2025-04-10 15:28:16 -0400

    third

---
+++ BasicCalculator/src/main/java/com/pluralsight/claculatorAppJava.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class claculatorAppJava {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("First_Number");
+        Double total = scanner.nextDouble();
+        System.out.print("second_Number");
+        Double sum = scanner.nextDouble();
+
+        Double Multiply  = (First_Number * Second_Numbe);
+        System.out.println("the Multiply is " + Multiply);
+    }
+}
